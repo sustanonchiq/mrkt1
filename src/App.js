a1588b864ee839b87e042e1568b3b093d1d87985
novemberrain00 <kurdukovr2564@gmail.com>
2025-04-17 01:06:45 +0300

    release commit#2

--- src/App.js
+++ src/App.js
@@ -10,26 +10,26 @@ import LinkImg from './assets/images/link.png'
 function App() {
 	return (
 		<div className='App'>
-			<img src={MrktImg} width={400} alt='mrkt' />
-			<img src={LinkImg} width={280} className='link-img' alt='@mrkt' />
+			<img src={MrktImg} width={160} alt='mrkt' />
+			<img src={LinkImg} width={120} className='link-img' alt='@mrkt' />
 			<h1>Единственный безопасный маркетплейс</h1>
-			<img src={BrickImg} width={150} alt='brick' />
+			<img src={BrickImg} width={80} alt='brick' />
 			<div className='invitation'>
 				<h3>Начните строить свое будущее вместе с друзьями</h3>
 				<div className='invitation-buttons'>
 					<button className='button'>
-						<img width={30} src={InviteImg} alt='invite' />
+						<img width={15} src={InviteImg} alt='invite' />
 						Пригласить (0)
 					</button>
 					<button className='button'>
-						<img width={50} src={CopyImg} alt='copy' />
+						<img width={30} src={CopyImg} alt='copy' />
 					</button>
 				</div>
 				<h3>Только друзья с подключенными кошельками</h3>
 			</div>
 			<div className='connect'>
 				<button className='button'>
-					<img width={50} src={WalletImg} alt='wallet' />
+					<img width={30} src={WalletImg} alt='wallet' />
 					Подключить кошелек
 				</button>
 				<h3>Подключите кошелек и получите ранние бонусы</h3>
