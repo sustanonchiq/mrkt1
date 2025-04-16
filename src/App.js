4a0e002f8a11151ad11671b50f54ef929ecbc4e3
novemberrain00 <kurdukovr2564@gmail.com>
2025-04-17 00:46:28 +0300

    release commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import logo from './logo.svg'
 import './App.css'
+
+import MrktImg from './assets/images/market.png'
+import BrickImg from './assets/images/brick.png'
+import InviteImg from './assets/images/invite.png'
+import CopyImg from './assets/images/copy.png'
+import WalletImg from './assets/images/wallet.png'
+import LinkImg from './assets/images/link.png'
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-					Learn React
-				</a>
-			</header>
+			<img src={MrktImg} width={400} alt='mrkt' />
+			<img src={LinkImg} width={280} className='link-img' alt='@mrkt' />
+			<h1>Единственный безопасный маркетплейс</h1>
+			<img src={BrickImg} width={150} alt='brick' />
+			<div className='invitation'>
+				<h3>Начните строить свое будущее вместе с друзьями</h3>
+				<div className='invitation-buttons'>
+					<button className='button'>
+						<img width={30} src={InviteImg} alt='invite' />
+						Пригласить (0)
+					</button>
+					<button className='button'>
+						<img width={50} src={CopyImg} alt='copy' />
+					</button>
+				</div>
+				<h3>Только друзья с подключенными кошельками</h3>
+			</div>
+			<div className='connect'>
+				<button className='button'>
+					<img width={50} src={WalletImg} alt='wallet' />
+					Подключить кошелек
+				</button>
+				<h3>Подключите кошелек и получите ранние бонусы</h3>
+			</div>
 		</div>
 	)
 }
